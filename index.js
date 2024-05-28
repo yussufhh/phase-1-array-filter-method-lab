3f21ebe9ea850cae247824b105ee2a635b469cf9
yussufhh <0793477632g@gmail.com>
2024-05-28 14:32:02 +0300

    final commit for array-filter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(){
+    const drivers =[ 'Bobby', 'bobby' ]
+return drivers;
+}
+function findMatching(names, query) {
+    const matches = names.filter(name => name.toLowerCase() === query.toLowerCase());
+    return matches.length > 0 ? matches : []; 
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name=== name)
+}
+
+
